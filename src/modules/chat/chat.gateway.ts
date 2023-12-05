be9080bee1c3a91dfd13259c679f6f83e3ad2452
Yan Castro <yancastrohenrique1@gmail.com>
2023-12-05 18:07:06 -0300

    testing socket

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -24,7 +24,7 @@ export interface Messages {
   };
 }
 
-@WebSocketGateway(8001, { cors: '*' })
+@WebSocketGateway({ cors: '*' })
 export class ChatGateway {
   constructor(
     private readonly chatService: ChatService,
